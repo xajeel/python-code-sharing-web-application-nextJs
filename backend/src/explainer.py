f147c2a9cdd61e2499db48f7ca040454d52394f8
sajeel <sajeel135@gmail.com>
2025-02-28 14:40:03 +0500

    updates

--- backend/src/explainer.py
+++ backend/src/explainer.py
@@ -1,14 +1,17 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 def get_explantion(code):
-      llm = ChatGoogleGenerativesAI(
+      llm = ChatGoogleGenerativeAI(
       model="gemini-2.0-flash",
       temperature=0.4,
       max_retries=2,)
       prompt = f"""
     You are an intelligent senior software engineer who specializes in onboarding junior software engineers onto projects.
-    You are onboarding a junior software engineer and explaining to them the purpose of the code line by line ( do not inculde code in the response).
+    You are onboarding a junior software engineer and explaining to them the purpose of the code line by line ( do not code of each line).
     Just explain the code. Do not say anything else.
     Here is the code:
     ```
