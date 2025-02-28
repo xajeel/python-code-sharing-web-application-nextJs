36629f2526a3645a06ad972f7a348df802a7f970
sajeel <sajeel135@gmail.com>
2025-02-28 15:09:15 +0500

    updates

---
+++ backend/app.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+from src.checker import is_python_code
+from src.explainer import get_explantion
+
+class Item(BaseModel):
+    code: str
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/api/check")
+def check_code(request: Item):
+    return {is_python_code(request.code)}
+
+@app.post("/api/explain")
+def explain_code(request: Item):
+    explanation = get_explantion(request.code)
+    return {"explanation": explanation.content}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
